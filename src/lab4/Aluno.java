3bbe526395a745a889e8808a0c0018afb5ee76a6
Yanka Yohana Pereira Das Neves 116211079 <yankaypd@lcc3-009.lcc.ufcg.edu.br>
2018-10-09 09:59:36 -0300

    oi

---
+++ src/lab4/Aluno.java
@@ -0,0 +1,70 @@
+package lab4;
+
+public class Aluno {
+
+	private String matricula;
+	private String nome;
+	private String curso;
+
+	public Aluno(String matricula, String nome, String curso) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.curso = curso;
+	}
+
+	public String getMatricula() {
+		return matricula;
+
+	}
+
+	public String getNome() {
+		return nome;
+
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((curso == null) ? 0 : curso.hashCode());
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (curso == null) {
+			if (other.curso != null)
+				return false;
+		} else if (!curso.equals(other.curso))
+			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	public String toString() {
+		return "Aluno: " + this.matricula + " - " + this.nome + " - " + this.curso;
+	}
+
+}
---
+++ src/lab4/Controle.java
@@ -0,0 +1,7 @@
+package lab4;
+
+public class Controle {
+	
+	
+
+}
