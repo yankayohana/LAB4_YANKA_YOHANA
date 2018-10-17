c54a026828f4f7e83830e6c326cbbb4c6fb8473d
Yanka Yohana Pereira Das Neves 116211079 <yankaypd@lcc3-118.lcc.ufcg.edu.br>
2018-10-17 13:07:25 -0300

    testes

--- testes/lab4/AlunoTest.java
+++ testes/lab4/AlunoTest.java
@@ -7,9 +7,37 @@ import org.junit.jupiter.api.Test;
 class AlunoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-		
-	}
+	void testAlunoConstrutorValido() throws Exception {
+		Aluno aluno = new Aluno("1", "yanka", "ciência da computação");
+		assertEquals("1", aluno.getMatricula());
+		assertEquals("yanka", aluno.getNome());
+		assertEquals("ciência da computação", aluno.getCurso());
+		assertEquals("Aluno: 1 - yanka - ciência da computação", aluno.toString());
 
+	}
+	
+	@Test
+	void testAlunoConstrutorInvalido() {
+		assertThrows(Exception.class, () -> new Aluno("", "yanka", "ciência da computação"));
+		assertThrows(Exception.class, () -> new Aluno(null, "yanka", "ciência da computação"));
+		assertThrows(Exception.class, () -> new Aluno("1", "", "ciência da computação"));
+		assertThrows(Exception.class, () -> new Aluno("1", null, "ciência da computação"));
+		assertThrows(Exception.class, () -> new Aluno("1", "yanka", ""));
+		assertThrows(Exception.class, () -> new Aluno("1", "yanka", null));
+	}
+	
+	@Test
+	void testEqualsTrue() throws Exception {
+		Aluno aluno1 = new Aluno("2", "yohana", "ciência da computação");
+		Aluno aluno2 = new Aluno("2", "yohana", "ciência da computação");
+		assertTrue(aluno1.equals(aluno2));
+	 
+	}
+	
+	@Test
+	void testEqualsFalse() throws Exception {
+		Aluno aluno1 = new Aluno("3", "yanka", "ciência da computação");
+		Aluno aluno2 = new Aluno("4", "yohana", "ciência da computação");
+		assertFalse(aluno1.equals(aluno2));
+	}
 }
---
+++ testes/lab4/ControleTest.java
@@ -0,0 +1,12 @@
+package lab4;
+
+import org.junit.Before;
+
+public class ControleTest {
+	
+	@Before
+	private Controle controle = new Controle();
+	
+	
+
+}
